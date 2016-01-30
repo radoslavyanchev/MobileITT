a9c791994aea882d53c0024819e94fe6d1cd2c96
Radoslav Yanchev <radoslav.yanchev86@gmail.com>
2016-01-30 22:27:54 +0200

    Add Car Class

---
+++ src/Car.java
@@ -0,0 +1,34 @@
+
+public class Car extends Vehicle {
+	static final String[] typeChoise = { "Van", "Convertible", "Hearse", "Station", "Wagon", "Compartment", "Ambulance",
+			"Minivan", "Pickup", "Sedan", "Strachey", "Limousine", "Hatchback" };
+	private String type;
+
+	public Car(int power, int km, String gearBox, String fuelEngine, String type) {
+		super(power, km, gearBox, fuelEngine);
+		try {
+			setType(type);
+		} catch (NotValidParameterException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) throws NotValidParameterException {
+		if (type != null) {
+			for (int i = 0; i < fuelEngineChoise.length; i++) {
+				if (type.equals(typeChoise[i])) {
+					this.type = type;
+					break;
+				}
+			}
+		} else {
+			throw new NotValidParameterException("Not Valid type of Car");
+		}
+
+	}
+
+}
